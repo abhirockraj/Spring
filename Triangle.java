b8d8a9e26f384cc44e500b4e8d5bc2ae8a2b720b
abhirockraj <abhinavkool19@gmail.com>
2023-08-14 18:06:43 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,65 @@
+package org.example;
+
+public class Triangle {
+//    private String type;
+//    private int height;
+//
+//    public Triangle(String type, int height) {
+//        this.type = type;
+//        this.height = height;
+//    }
+//
+//    public int getHeight() {
+//        return height;
+//    }
+//
+//    public void setHeight(int height) {
+//        this.height = height;
+//    }
+//
+//    public Triangle(String type) {
+//        this.type = type;
+//    }
+//
+//    public String getType() {
+//        return type;
+//    }
+//
+//    public void setType(String type) {
+//        this.type = type;
+//    }
+
+    Point pointA;
+    Point pointB;
+    Point pointC;
+
+    public Point getPointA() {
+        return pointA;
+    }
+
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
+    }
+
+    public Point getPointB() {
+        return pointB;
+    }
+
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
+    }
+
+    public void draw(){
+
+//        System.out.println(getType()+" Triangle drawn "+ getHeight());
+        System.out.println("");
+    }
+}
